8c7b283eaf3faa5f7b0b036e2ac5b363da1dbddc
Sanjay Sarkar <sanjaysarkar430@gmail.com>
2021-11-16 23:25:14 +0530

    ghj

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import { Auth } from "./redux/action/Auth";
 import { Loading } from "./redux/action/Loading";
 
 function App(props) {
-  setTimeout(() => {
-    props.Auth({ isLogged: "LOGIN", token: 123456 });
-    props.Loading(true);
-  }, 2000);
-  
+  // setTimeout(() => {
+  //   props.Auth({ isLogged: "LOGIN", token: 123456 });
+  //   props.Loading(true);
+  // }, 2000);
+
   return (
     <div className="container">
       <div className="">
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-export default function Button() {
+export default function Button(props) {
+  
   return (
-    <button class="mb-2 md:mb-0 bg-red-500 border border-red-500 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-full hover:shadow-lg hover:bg-red-600">
+    <button className="mb-2 md:mb-0 bg-red-500 border border-red-500 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-full hover:shadow-lg hover:bg-red-600">
       Delete
     </button>
   );
 }
+
+Button.propTypes = {
+  type: PropTypes.string.isRequired,
+  size: PropTypes.string,
+};
+
+Button.defaultProps = {
+  type: "primary",
+  size: "md",
+};
