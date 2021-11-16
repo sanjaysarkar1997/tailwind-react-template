e93138f9297f5b283971cda9dcbc37e1c723a39c
Sanjay Sarkar <sanjaysarkar430@gmail.com>
2021-11-16 22:43:23 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,41 +1,19 @@
 import { connect } from "react-redux";
+import Button from "./components/Button";
 import { Auth } from "./redux/action/Auth";
 import { Loading } from "./redux/action/Loading";
 
 function App(props) {
   setTimeout(() => {
-    // props.Auth({ isLogged: "LOGIN", token: 123456 });
-    // props.Loading(true);
+    props.Auth({ isLogged: "LOGIN", token: 123456 });
+    props.Loading(true);
   }, 2000);
-
+  
   return (
     <div className="container">
-      {props.LOGGED ? (
-        <figure class="md:flex bg-gray-100 rounded-xl p-8 md:p-0">
-          <img
-            class="w-32 h-32 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
-            src="https://tailwindcss.com/_next/static/media/sarah-dayan.a8ff3f1095a58085a82e3bb6aab12eb2.jpg"
-            alt=""
-            width="384"
-            height="512"
-          />
-          <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
-            <blockquote>
-              <p class="text-lg font-semibold">
-                “Tailwind CSS is the only framework that I've seen scale on
-                large teams. It’s easy to customize, adapts to any design, and
-                the build size is tiny.”
-              </p>
-            </blockquote>
-            <figcaption class="font-medium">
-              <div class="text-cyan-600">Sarah Dayan</div>
-              <div class="text-gray-500">Staff Engineer, Algolia</div>
-            </figcaption>
-          </div>
-        </figure>
-      ) : (
-        <div>Loading</div>
-      )}
+      <div className="">
+        <Button />
+      </div>
     </div>
   );
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Button() {
+  return (
+    <button class="mb-2 md:mb-0 bg-red-500 border border-red-500 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-full hover:shadow-lg hover:bg-red-600">
+      Delete
+    </button>
+  );
+}
